4d1a311a3cf91964ab38250e1ff60b7e6e15eb2a
Julian Krieger <julian.krieger@ergosign.de>
2021-01-06 18:42:46 +0100

    read from env

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,26 @@
 use serde::Deserialize;
+use std::env;
+use config::{ConfigError, Config, File, Environment};
 
 #[derive(Deserialize, Clone)]
-pub struct Config {
+pub struct Settings {
     pub serving_layer_port: Option<String>,
     pub serving_layer_ip: Option<String>,
     pub calculate_adress: String,
 }
-pub fn get_config() -> Config {
-    let config_str = include_str!("../Config.toml");
-    let config: Config = toml::from_str(&config_str).unwrap();
-    config
+
+pub fn get_config() -> Settings {
+    Settings::new().unwrap()
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        let mut s = Config::new();
+
+        s.merge(File::with_name("Config"))?;
+
+        s.merge(Environment::new())?;
+
+        s.try_into()
+    }
 }
