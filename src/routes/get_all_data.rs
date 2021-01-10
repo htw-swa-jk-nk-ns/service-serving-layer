456d20a3a33cdaf5454cb70308f4c4cb3d73165d
Julian Krieger <julian.krieger@ergosign.de>
2021-01-10 13:49:09 +0100

    reword allData to all

--- src/routes/get_all_data.rs
+++ src/routes/get_all_data.rs
@@ -12,7 +12,7 @@ pub struct Data {
     id: u32
 }
 
-#[get("/getAllData")]
+#[get("/all")]
 pub async fn exec() -> actix_web::Result<HttpResponse, APIError> {
     let res =
         crate::helpers::get::<Vec<Data>>(get_config().raw_data_adress, "all".to_string())
